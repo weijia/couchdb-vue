461e7efa11ff00ffdf79334be8944c566440228c
weijia <richardwangwang@gmail.com>
2023-07-25 14:57:55 +0800

    1 file modified

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+
+import PouchDB from 'pouchdb-browser'
+import PouchFind from 'pouchdb-find'
+import PouchLiveFind from 'pouchdb-live-find'
+
+PouchDB.plugin(PouchFind)
+PouchDB.plugin(PouchLiveFind)
+
 createApp(App).mount('#app')
