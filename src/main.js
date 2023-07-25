78ea233c7db6084e010ba6df84c4e38e3255343a
weijia <richardwangwang@gmail.com>
2023-07-25 15:38:42 +0800

    2 files modified

--- src/main.js
+++ src/main.js
@@ -6,8 +6,17 @@ import App from './App.vue'
 import PouchDB from 'pouchdb-browser'
 import PouchFind from 'pouchdb-find'
 import PouchLiveFind from 'pouchdb-live-find'
+import Vue from 'vue';
+import PouchVue from 'pouch-vue';
 
 PouchDB.plugin(PouchFind)
 PouchDB.plugin(PouchLiveFind)
 
 createApp(App).mount('#app')
+
+Vue.use(PouchVue, {
+  pouch: PouchDB,    // optional if `PouchDB` is available on the global object
+  defaultDB: 'remoteDbName',  // this is used as a default connect/disconnect database
+  optionDB: {}, // this is used to include a custom fetch() method (see TypeScript example)
+  debug: '*' // optional - See `https://pouchdb.com/api.html#debug_mode` for valid settings (will be a separate Plugin in PouchDB 7.0)
+});
